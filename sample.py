7bfddf3fce1c96deb0cba8aab508befa03d8bc87
kavishka721 <IT19156248@my.sliit.lk>
2022-11-15 16:51:17 +0530

    python backend added for deploy

---
+++ sample.py
@@ -0,0 +1,14 @@
+import torch
+print(torch.__version__)
+print(torch.cuda.is_available())
+
+print(torch.version.cuda)
+
+import torchvision
+print(torchvision.__version__)
+
+# nvcc --version
+
+# 1.12.1+cpu
+# False
+# 0.13.1+cpu
